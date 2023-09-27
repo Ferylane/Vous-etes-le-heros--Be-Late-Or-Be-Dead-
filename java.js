73e00fed6372be5503aa0ac56b9fce7f72acf075
YanniV2 <112189503+Ferylane@users.noreply.github.com>
2023-09-26 22:54:19 -0400

    changement de shemas et js

---
+++ java.js
@@ -0,0 +1,56 @@
+chapters = {
+    reveil: {
+
+    },
+    transport: {
+
+    },
+    continuer: {
+
+    },
+    appel: {
+
+    },
+    einstein: {
+
+    },
+    enfant: {
+
+    },
+    voiture: {
+
+    },
+    cave: {
+
+    },
+    velo: {
+
+    },
+    pistolet: {
+
+    },
+    transport2: {
+
+    },
+    continuer2: {
+
+    },
+    retard: {
+
+    },
+    vitesse: {
+
+    },
+    pasDenergie: {
+
+    },
+    manger: {
+
+    },
+    theRock: {
+
+    },
+    lenteur:{
+
+    },
+};
