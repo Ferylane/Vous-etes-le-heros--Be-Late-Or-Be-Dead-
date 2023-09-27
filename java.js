590022c2b8030bc51b65bc5c00bb71cdab60136c
YanniV2 <112189503+Ferylane@users.noreply.github.com>
2023-09-26 23:22:43 -0400

    titre de chapitre

--- java.js
+++ java.js
@@ -1,56 +1,60 @@
 chapters = {
     reveil: {
-
+        'reveil'
     },
     transport: {
-
+        'moyen de transport'
     },
     continuer: {
-
+        'continuer?'
     },
     appel: {
-
+        'vous recevez un appelle'
     },
     einstein: {
-
+        'bravo'
     },
     enfant: {
-
+        'tout mes felicitations'
     },
     voiture: {
-
+        'rencontre innatendue'
     },
     cave: {
-
+        'Vous etes mort'
     },
     velo: {
-
+        'deuxieme rencontre innatendue'
     },
     pistolet: {
-
+        'Vous etes mort'
     },
     transport2: {
-
+        'moyen de transport'
     },
     continuer2: {
-
+        'continuer?'
     },
     retard: {
-
+        'Vous arriver en retard'
     },
     vitesse: {
-
+        'a quelle vitesse'
     },
     pasDenergie: {
-
+        'vous avez pas denergie'
     },
     manger: {
-
+        'avez vous manger?'
     },
     theRock: {
-
+        'pourquoi mentez vous?'
     },
     lenteur:{
-
+        'vous etes trop lent'
     },
+};
+
+function goToChapter(chapter){
+
 };
